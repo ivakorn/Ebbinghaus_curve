89d13ac60ad7a361badfc432a6cd9ff449130c8f
ivan kornaukhov <kiadu@bk.ru>
2025-08-26 00:52:15 +0500

    update models

--- database/models.py
+++ database/models.py
@@ -1,18 +1,17 @@
 from database.base import Base
 
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import Integer, String
+from sqlalchemy import Integer, String, func
 from datetime import datetime
 
-from typing import List
-
-
 class Word(Base):
-    __tablename__ = 'dict_mean_link'
+    __tablename__ = 'words'
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     text: Mapped[str] = mapped_column(String)
     wrong_count: Mapped[int] = mapped_column(Integer, default='0')
     correct_count: Mapped[int] = mapped_column(Integer, default='0')
     last_review: Mapped[datetime] = mapped_column(server_default=func.now(),
                                                   onupdate=func.now())
+
+
 
