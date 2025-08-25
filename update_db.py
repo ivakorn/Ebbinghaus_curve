8f1074386511fddbf96b89d97607436dc3cf1311
ivan kornaukhov <kiadu@bk.ru>
2025-08-26 00:56:11 +0500

    creating and filling the database

--- update_db.py
+++ update_db.py
@@ -4,27 +4,33 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from database.base import Base
 from database.requests import Database
 
+import sys
 
-async def main_async():
+#if you are working on the windows
+if sys.platform.lower().startswith("win"):
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+async def main():
     engine = create_async_engine(url="postgresql+psycopg://superuser:superpassword@127.0.0.1/mydatabase",
                                  echo=True)
 
-    #old version base drop
-    #new base create
+
+
     async with engine.begin() as conn:
+        # old version base drop
         await conn.run_sync(Base.metadata.drop_all)
+        # new base create
         await conn.run_sync(Base.metadata.create_all)
 
     async_session = async_sessionmaker(engine, expire_on_commit=False)
-
     db = Database(async_session)
 
+    #insert all words to database
     await db.insert_objects()
 
-    # for AsyncEngine created in function scope, close and
-    # clean-up pooled connections
+    # for AsyncEngine created in function scope, close and clean-up pooled connections
     await engine.dispose()
 
 
 if __name__ == '__main__':
-    asyncio.run(main_async())
+    asyncio.run(main())
